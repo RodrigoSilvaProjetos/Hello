c67c4f648eb7c7ab556c71535284c452b341418d
Rodrigo da Silva <rd966938@gmail.com>
2026-09-06 20:40:01 -0300

    aula operadores Numericos

--- src/operadores_numericos.java
+++ src/operadores_numericos.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class operadores_numericos {
@@ -9,17 +10,24 @@ public class operadores_numericos {
         var value1 = scanner.nextInt();//numero inteiro
         System.out.println(texto);
         var value2 = scanner.nextInt();
+        // value += 12; ex 10+12 = 22
+        // print(value++ ou --value) para direita o encremento fica para proxima linha
+        // para esquerda ++value ou --value o valor se resolve na hora
 
         // para numeros quebrados
         // var value1 = scanner.nextFloat();
         var valor = 20+2 +(20*2);// * e / sempre vem primeiro
-        //em parentese sempre tera preferencia
+        //em parentese sempre tera a preferencia
 
         System.out.printf("%s + %s = %s\n",value1, value2, value1 + value2);
         System.out.printf("%s - %s = %s\n",value1, value2, value1 - value2);
         System.out.printf("%s / %s = %s\n",value1, value2, value1 / value2);
         System.out.printf("%s %% %s = %s\n",value1, value2, value1 % value2);
         System.out.printf("%s * %s = %s\n ",value1, value2, value1 * value2);
+        //para pegar a raiz quadrada de value1
+        System.out.printf("A raiz quadrada de %s é %s \n",value1, Math.sqrt(value1));
+        //para pegar a potencia de um numero elevado a qq potencia
+        System.out.printf("A potência de %s é %s \n ", value1, Math.pow(value1,value2));
 
     }
 }
