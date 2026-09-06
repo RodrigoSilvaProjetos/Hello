2bd1f42aff9f9aeccca1432e46385bd115d4d3fb
Rodrigo da Silva <rd966938@gmail.com>
2026-09-06 12:31:55 -0300

    aula01

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Main {
+    /*comentario para varias linhas*/
+    //declaração de variaveis
+
+    // variavel global | TAMBEM UMA CONSTANTE
+    private final static String WELCOME_MENSSAGE = "olá, informe o seu nome: ";
+
+    public static void main(String[] args){
+        //System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Olá, informe o seu nome: ");
+        System.out.println(WELCOME_MENSSAGE);
+        String name = scanner.next();
+        // var name = scanner.next(); // outro metódo de criar vaiavel
+
+        System.out.println("Informe a sua idade: ");
+        int age = scanner.nextInt();
+
+        //imprimindo dados
+        System.out.println("Olá " + name + " sua idade é:" + age);
+        System.out.printf("Olá %s sua idade é:%s",name, age);
+
+
+
+
+    }
 }
